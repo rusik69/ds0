01995a6cf8468833e36f2a3a8364cfa113daace3
Ruslan Gustomiasov <ruslan.gustomyasov@gmail.com>
2023-04-03 21:37:42 +0200

    commit

--- pkg/ns/db/connect.go
+++ pkg/ns/db/connect.go
@@ -11,12 +11,21 @@ import (
 
 // Connect connects to the database.
 func Connect() error {
-	cli, err := clientv3.New(clientv3.Config{
-		Endpoints:   []string{"http://" + env.NSEnvInstance.ETCDHost + ":" + env.NSEnvInstance.ETCDPort},
-		DialTimeout: 10 * time.Second,
-		Username:    env.NSEnvInstance.ETCDUser,
-		Password:    env.NSEnvInstance.ETCDPass,
-	})
+	var conf clientv3.Config
+	if env.NSEnvInstance.ETCDUser != "" {
+		conf = clientv3.Config{
+			Endpoints:   []string{"http://" + env.NSEnvInstance.ETCDHost + ":" + env.NSEnvInstance.ETCDPort},
+			DialTimeout: 10 * time.Second,
+			Username:    env.NSEnvInstance.ETCDUser,
+			Password:    env.NSEnvInstance.ETCDPass,
+		}
+	} else {
+		conf = clientv3.Config{
+			Endpoints:   []string{"http://" + env.NSEnvInstance.ETCDHost + ":" + env.NSEnvInstance.ETCDPort},
+			DialTimeout: 10 * time.Second,
+		}
+	}
+	cli, err := clientv3.New(conf)
 	if err != nil {
 		logrus.Println("Error:", err)
 		return err
