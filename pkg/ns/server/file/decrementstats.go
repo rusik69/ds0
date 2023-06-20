bdf533d14eab4c6865416eb4c7a9df2a3cce6d90
Ruslan Gustomiasov <ruslan.gustomyasov@gmail.com>
2023-06-20 18:59:04 +0200

    commit

--- pkg/ns/server/file/decrementstats.go
+++ pkg/ns/server/file/decrementstats.go
@@ -1,9 +1,13 @@
 package file
 
-import dbfile "github.com/rusik69/ds0/pkg/ns/db/file"
+import (
+	dbfile "github.com/rusik69/ds0/pkg/ns/db/file"
+	"github.com/sirupsen/logrus"
+)
 
 // decrementStats increments the stats.
 func decrementStats(fileSize uint64) error {
+	logrus.Println("Decrement stats")
 	filesInfo, err := dbfile.GetFilesInfo()
 	if err != nil {
 		return err
--- pkg/ns/server/file/incrementstats.go
+++ pkg/ns/server/file/incrementstats.go
@@ -1,9 +1,13 @@
 package file
 
-import dbfile "github.com/rusik69/ds0/pkg/ns/db/file"
+import (
+	dbfile "github.com/rusik69/ds0/pkg/ns/db/file"
+	"github.com/sirupsen/logrus"
+)
 
 // incrementStats increments the stats.
 func incrementStats(fileSize uint64) error {
+	logrus.Println("Increment stats")
 	filesInfo, err := dbfile.GetFilesInfo()
 	if err != nil {
 		return err
