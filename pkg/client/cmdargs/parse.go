c336c79695105749a0872dc57f18f34d103fea64
Ruslan Gustomiasov <ruslan.gustomyasov@gmail.com>
2023-08-04 23:54:43 +0200

    commit

--- pkg/client/cmdargs/parse.go
+++ pkg/client/cmdargs/parse.go
@@ -8,9 +8,9 @@ import (
 
 // Parse parses the command line arguments.
 func Parse() {
-	hostName := flag.String("host", "localhost", "ns hostname")
+	var hostName *string
+	flag.StringVar(hostName, "host", "localhost", "ns hostname")
 	port := flag.Int("port", 6969, "port number")
-	flag.Parse()
 	if len(os.Args) < 2 {
 		panic("specify action: upload, download, addnode, removenode, listnodes, nodestats")
 	}
