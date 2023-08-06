b35409939ffdddf8364ec07d4f5c77840d5f4141
Ruslan Gustomiasov <ruslan.gustomyasov@gmail.com>
2023-08-06 19:42:58 +0200

    commit

--- pkg/client/cmdargs/parse.go
+++ pkg/client/cmdargs/parse.go
@@ -20,37 +20,37 @@ func Parse() CmdArgs {
 	var arg1, arg2, arg3 string
 	switch cmd {
 	case "upload":
-		if len(args) < 2 {
+		if len(args) < 3 {
 			panic("specify source file and destination path")
 		}
-		arg1 = args[0]
-		arg2 = args[1]
+		arg1 = args[1]
+		arg2 = args[2]
 	case "download":
-		if len(args) < 2 {
+		if len(args) < 3 {
 			panic("specify source path and destination file")
 
 		}
-		arg1 = args[0]
-		arg2 = args[1]
+		arg1 = args[1]
+		arg2 = args[2]
 	case "addnode":
-		if len(args) < 3 {
+		if len(args) < 4 {
 			panic("specify node name, hostname and port")
 		}
-		arg1 = args[0]
-		arg2 = args[1]
-		arg3 = args[2]
+		arg1 = args[1]
+		arg2 = args[2]
+		arg3 = args[3]
 	case "removenode":
-		if len(args) < 2 {
+		if len(args) < 3 {
 			panic("specify node name and port")
 		}
-		arg1 = args[0]
-		arg2 = args[1]
+		arg1 = args[1]
+		arg2 = args[2]
 	case "nodestats":
-		if len(args) < 2 {
+		if len(args) < 3 {
 			panic("specify hostname and port")
 		}
-		arg1 = args[0]
-		arg2 = args[1]
+		arg1 = args[1]
+		arg2 = args[2]
 	}
 	return CmdArgs{
 		Cmd:      cmd,
