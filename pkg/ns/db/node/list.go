0dcf678266f08202c5094c6927efb6701b3d65b2
Ruslan Gustomiasov <ruslan.gustomyasov@gmail.com>
2023-10-14 20:02:51 +0200

    commit

--- pkg/ns/db/node/list.go
+++ pkg/ns/db/node/list.go
@@ -7,13 +7,12 @@ import (
 
 	"github.com/rusik69/ds0/pkg/ns/db"
 	"github.com/rusik69/ds0/pkg/ns/env"
-	"github.com/sirupsen/logrus"
 	clientv3 "go.etcd.io/etcd/client/v3"
 )
 
 // List returns the list of nodes in the database.
 func List() ([]env.NodeInfo, error) {
-	logrus.Println("List nodes")
+	//logrus.Println("List nodes")
 	var nodes []env.NodeInfo
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	resp, err := db.DB.Get(ctx, "/nodes/", clientv3.WithPrefix())
